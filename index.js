0479c7f8aa49a89fa1ca6031588e90a69c82a061
mdmohiuddin10 <hmmohiuddinctg1997@gmail.com>
2023-10-25 12:12:53 +0600

    solve some errors

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const port = process.env.PORT || 5001
 app.use(cors())
 app.use(express.json())
 
-// zhOuUvQ4ZkxCN8n5
-// mobileShop
 
 
 
-const uri = "mongodb+srv://mobileShop:zhOuUvQ4ZkxCN8n5@cluster0.3gsgkud.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3gsgkud.mongodb.net/?retryWrites=true&w=majority`;
+
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
